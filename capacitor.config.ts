4f584827e71c87c1a36d8eb24ec9ca1e5e1027a2
iconsWork <iconstanzav1@gmail.com>
2024-06-10 17:31:48 -0400

    creacion del proyecto

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '06_burger_queen',
+  webDir: 'www'
+};
+
+export default config;
