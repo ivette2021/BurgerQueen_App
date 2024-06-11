430467b4db85a467a9c8b86949397adddce1bc60
iconsWork <iconstanzav1@gmail.com>
2024-06-11 12:32:04 -0400

    creacion de paginas, -instalacion de @capacitor/device - instalar ngx-translate -configuracion app.module.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: '06_burger_queen',
-  webDir: 'www'
+  appName: 'Burger Queen',
+  webDir: 'www',
 };
 
 export default config;
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,21 +2,46 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // {
+  //   path: 'home',
+  //   loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  // },
+
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'categories',
+    loadChildren: () =>
+      import('./pages/categories/categories.module').then(
+        (m) => m.CategoriesPageModule
+      ),
+  },
+  {
+    path: 'list-products',
+    loadChildren: () =>
+      import('./pages/list-products/list-products.module').then(
+        (m) => m.ListProductsPageModule
+      ),
+  },
+  {
+    path: 'product',
+    loadChildren: () =>
+      import('./pages/product/product.module').then((m) => m.ProductPageModule),
+  },
+  {
+    path: 'pay',
+    loadChildren: () =>
+      import('./pages/pay/pay.module').then((m) => m.PayPageModule),
   },
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    redirectTo: 'categories',
+    pathMatch: 'full',
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
