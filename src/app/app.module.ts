69a7f57122bec0042415d9967fb0f7c67040ae8e
iconsWork <iconstanzav1@gmail.com>
2024-06-13 20:14:18 -0400

    eliminamos la carpeta homne instalamos capacitor device y ngx translate iniciamos la app eligiendo un idioma se probo ngx-translate agregar los productos de nuestra app componente toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { ToolbarComponent } from './pages/shared/toolbar/toolbar.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, '/assets/i18n/', '.json');
@@ -28,6 +29,7 @@ export function HttpLoaderFactory(http: HttpClient) {
         deps: [HttpClient],
       },
     }),
+    ToolbarComponent,
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
