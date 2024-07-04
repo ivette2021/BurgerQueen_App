df533e103b421dc814fb9128e5ddc8f00bb2d077
iconsWork <iconstanzav1@gmail.com>
2024-07-04 12:15:21 -0400

    - fix checkbox in section product -show arrow back - contents of our Menu

--- src/src/app/services/user-order.service.ts
+++ src/src/app/services/user-order.service.ts
@@ -43,6 +43,15 @@ export class UserOrderService {
   getProducts() {
     return this.order.products;
   }
+  numProducts() {
+    if (this.order && this.order.products.length > 0) {
+      return this.order.products.reduce(
+        (acum: number, value: QuantityProduct) => value.quantity + acum,
+        0
+      );
+    }
+    return 0;
+  }
   async addProduct(product: Product) {
     const productFound = this.searchProduct(product);
 
@@ -60,5 +69,8 @@ export class UserOrderService {
     return this.order.products.find((p: QuantityProduct) =>
       isEqual(p.product, product)
     );
+  }
+  hasUser() {
+    return this.order && this.order.user;
   }
 }
